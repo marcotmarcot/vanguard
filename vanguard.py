c1548f8e67bfd203a506550b5ba9031a31007633
Marco Gontijo <marcotmarcot@gmail.com>
2024-01-13 14:08:35 +0000

    ticker

--- vanguard.py
+++ vanguard.py
@@ -3,7 +3,7 @@
 import json
 from urllib.request import urlopen
 
-PRODUCT_CONSTANT_FIELDS = ["name", "managementType", "riskLevel"]
+PRODUCT_CONSTANT_FIELDS = ["name", "managementType", "riskLevel", "ocfValue"]
 CURRENCIES = {
     "EUR": 0.86,
     "GBP": 1,
@@ -27,18 +27,23 @@ def average_return(returns: list):
     total = 1.0
     for ret in reversed(returns):
         total *= 1 + ret["monthPercent"] / 100
-    return total ** (12 / len(returns))
+    return total ** (12 / len(returns)) - 1
 
 
 def main():
     products = json.load(urlopen("https://www.vanguardinvestor.co.uk/api/productList"))
     products_by_name = {}
     for product in products:
+        if "hedged" in product["id"]:
+            continue
         name = product["name"]
         products = products_by_name.get(name, [])
         products.append(product)
         products_by_name[name] = products
     for name, products in products_by_name.items():
+        if len(products) > 2:
+            print(f"{name} contains {len(products)} products")
+            exit(1)
         for i in range(1, len(products)):
             for field in PRODUCT_CONSTANT_FIELDS:
                 if products[0][field] != products[i][field]:
@@ -47,6 +52,7 @@ def main():
         assets = 0.0
         ret = 0.0
         ret_len = 0
+        ticker = ""
         for product in products:
             try:
                 fund = json.load(urlopen("https://www.vanguardinvestor.co.uk/api/funds/" + product["id"]))
@@ -55,12 +61,14 @@ def main():
                 returns = fund["fundData"]["annualNAVReturns"]["returns"]
                 if len(returns) > ret_len:
                     ret = average_return(returns)
+                if ticker == "" or product.get("shareClassCode", "") == "ACCM":
+                    ticker = fund["ticker"]
             except Exception as e:
                 print(product["id"])
                 raise e
-        for field in PRODUCT_CONSTANT_FIELDS:
-            print(products[0][field], end="\t")
-        print(f"{assets}\t{ret}")
+        product = products[0]
+        print(f"{product['name']}\t{ticker}\t{product['riskLevel']}\t{product['ocfValue']}%\t{assets}\t" +
+              f"{product['managementType']}\t{ret}%")
 
 
 if __name__ == "__main__":
