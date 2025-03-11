7f675ce973284a82bc1b27a33e60636f7f538421
Marco Gontijo <marcotmarcot@gmail.com>
2025-03-11 10:05:49 +0000

    no first date

--- investengine_etfs_parse.py
+++ investengine_etfs_parse.py
@@ -15,24 +15,15 @@ def date_and_price(day):
     return (datetime.strptime(day.get('date'), '%Y-%m-%d'), float(day.get('share_price')))
 
 
-def return_and_duration(max_first_date, daily):
+def return_and_duration(daily):
     first = date_and_price(daily[0])
-    first_for_average = None
-    for day in daily:
-        if day.get('date') < max_first_date:
-            continue
-        first_for_average = date_and_price(day)
-        break
     last = date_and_price(daily[-1])
-    years = (last[0] - first_for_average[0]).days / 365.25
-    return ((last[1] / first_for_average[1]) ** (1 / years) - 1, (last[0] - first[0]).days)
+    years = (last[0] - first[0]).days / 365.25
+    return ((last[1] / first[1]) ** (1 / years) - 1, years)
 
 
 def main():
-    etfs = []
-    max_first_date = '1000-01-01'
     for file in os.listdir(ETFS):
-        etf = {}
         with open(os.path.join(ETFS, file), 'r', encoding='utf-8') as f:
             html = f.read()
         soup = BeautifulSoup(html, 'html.parser')
@@ -40,21 +31,15 @@ def main():
         json_string = script_tag.string
         json_data = json.loads(json_string)
         security = json_data['props']['pageProps']['security']
-        etf['title'] = security.get('title')
-        etf['ticker'] = security.get('ticker')
-        etf['ter'] = float(security.get('ter')) / 100
+        print(security.get('title'), end='\t')
+        print(security.get('ticker'), end='\t')
+        print(float(security.get('ter')) / 100, end='\t')
         properties = security['properties']
-        etf['fund_size'] = properties.get('fund_size_mm')
-        etf['share_class_size'] = properties.get('share_class_size_mm')
-        etf['daily'] = security['history']['daily']
-        first_date = etf['daily'][0].get('date')
-        if first_date > max_first_date:
-            max_first_date = first_date
-        etfs.append(etf)
-
-    for etf in etfs:
-        (ret, days) = return_and_duration(max_first_date, etf['daily'])
-        print(f'{etf["title"]}\t{etf["ticker"]}\t{etf["ter"]}\t{etf["fund_size"]}\t{etf["share_class_size"]}\t{ret}\t{days}')
+        print(properties.get('fund_size_mm'), end='\t')
+        print(properties.get('share_class_size_mm'), end='\t')
+        (ret, days) = return_and_duration(security['history']['daily'])
+        print(ret, end='\t')
+        print(days)
 
 
 if __name__ == "__main__":
